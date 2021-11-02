ff76754179d0ccc8f2823ed1c36ec1a870a1a699
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-02 14:06:29 +0100

    1-Structure du site, E4-Liste de livres

--- biblio/livres.php
+++ biblio/livres.php
@@ -1,6 +1,30 @@
 <?php ob_start(); ?>
 
-<p>ici le contenu de ma page listant les livres</p>
+<table class="table text-center">
+    <tr class="table-dark">
+        <th>Image</th>
+        <th>Titre</th>
+        <th>Nombre de pages</th>
+        <th colspan="2">Actions</th>
+    </tr>
+    <tr>
+        <td><img src="public/images/algo.png" width="60px;"></td>
+        <td class="align-middle">Le virus asiatique</td>
+        <td class="align-middle">200</td>
+        <td class="align-middle"><a href="" class="btn btn-warning">Modifier</a></td>
+        <td class="align-middle"><a href="" class="btn btn-danger">Supprimer</a></td>
+    </tr>
+
+    <tr>
+        <td><img src="public/images/virus.png" width="60px;"></td>
+        <td class="align-middle">Algorithmique selon H2PROG</td>
+        <td class="align-middle">300</td>
+        <td class="align-middle"><a href="" class="btn btn-warning">Modifier</a></td>
+        <td class="align-middle"><a href="" class="btn btn-danger">Supprimer</a></td>
+    </tr>
+</table>
+<a href="" class="btn btn-success d-block">Ajouter</a>
+
 
 <?php
     $titre = "Les livres de la bibliothèque";
